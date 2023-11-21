da57e4ac67d954a0701ca2dc48504d1549d1df1a
Aman Ullah Akhand <87015561+AmanUllahAkhand@users.noreply.github.com>
2023-11-21 23:15:28 +0600

    Setup a Custom Theme for Light and Dark Theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:login_flutter_app/src/utils/theme/theme.dart';
 
 void main() => runApp(const MyApp());
 
@@ -9,10 +10,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(brightness: Brightness.light),
-      darkTheme: ThemeData(brightness: Brightness.dark),
-      themeMode: ThemeMode.dark,
-      home:  AppHome(),
+      theme:TAppTheme.lightTheme,
+      darkTheme:TAppTheme.darkTheme,
+      themeMode: ThemeMode.system,
+      home:  const AppHome(),
     );
   }
 }
@@ -24,16 +25,17 @@ class AppHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('.appable/'),leading: const Icon(Icons.ondemand_video),),
+      backgroundColor: Theme.of(context).dialogBackgroundColor,
+      appBar: AppBar(title: const Text('.appable/'),leading: const Icon(Icons.ondemand_video),),
       floatingActionButton: FloatingActionButton(onPressed: () {  },
       child: const Icon(Icons.add_shopping_cart_outlined),),
       body: Padding(
         padding: const EdgeInsets.all(20),
         child: ListView(
           children: [
-             Text('Heading',style: Theme.of(context).textTheme.headlineMedium, ),
+             Text('Heading',style: Theme.of(context).textTheme.headline2, ),
              Text('Sub-heading',style: Theme.of(context).textTheme.subtitle2,),
-             Text('Paragraph',style: Theme.of(context).textTheme.bodyText1,),
+             Text('Paragraph',style: Theme.of(context).textTheme.bodySmall,),
             ElevatedButton(onPressed: (){}, child: const Text('ElevatedButton'),),
             OutlinedButton(onPressed: (){}, child: const Text('OutlinedButton'),),
             const Padding(padding: EdgeInsets.all(20.0),
---
+++ lib/src/utils/theme/theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:login_flutter_app/src/utils/theme/widget_themes/text_theme.dart';
+
+class TAppTheme{
+  static ThemeData lightTheme = ThemeData(
+      brightness: Brightness.light,
+      textTheme:TTextTheme.lightTextTheme,
+  );
+
+  static ThemeData darkTheme = ThemeData(
+      brightness: Brightness.dark,
+      textTheme:TTextTheme.darkTextTheme,
+  );
+
+}
